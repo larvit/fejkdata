58f42dd8ca37bc7b3b2447aa7c45404d3e8f3636
Lilleman auf Larv <lilleman@larvit.se>
2026-09-22 21:35:22 +0200

    Draw the grow test's hold set through lazyHoldSet

--- template_test.go
+++ template_test.go
@@ -316,7 +316,8 @@ func TestGrowIsALowerBound(t *testing.T) {
 			t.Fatalf("format %q did not compile to a template", format)
 		}
 		for i := 0; i < 50; i++ {
-			if got := len(expand(f.rand, tmpl, renderScope{set: &holdSet{}})); got < tmpl.grow {
+			set := lazyHoldSet(f.rand)
+			if got := len(expand(f.rand, tmpl, renderScope{set: &set})); got < tmpl.grow {
 				t.Errorf("format %q: expand emitted %d bytes, below grow %d", format, got, tmpl.grow)
 			}
 		}
