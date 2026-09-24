ee8cd0b8420a42d1bba0cf176f1ef3ab0c98d3d2
Lilleman auf Larv <lilleman@larvit.se>
2026-09-24 09:54:05 +0200

    Name the pin set's inline array and entered's flag apart from pins, and file the draw half of the pin model as its own item

--- drawfence.go
+++ drawfence.go
@@ -229,7 +229,7 @@ func (w *drawWalk) walk(n node, at drawAt) {
 	}
 	for _, e := range renderEdges(n) {
 		if cell, isCell := e.to.(*template); isCell && cell.cellOf != nil {
-			if at.alt.excludes(cell) {
+			if at.alt.clash(cell.cellOf, cell.cellRow) != nil {
 				continue
 			}
 			in := at
