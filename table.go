c72dc0251d1bf2f872c44efd0b0d9434db297ea5
Lilleman auf Larv <lilleman@larvit.se>
2026-09-24 22:37:32 +0200

    Compile a format holding a reference once, at link

--- table.go
+++ table.go
@@ -335,7 +335,7 @@ func (t *table) compileWhole(format string) error {
 	}
 	t.format = &template{format: format, fields: t.fields, repeat: 1, record: true, table: t}
 	t.whole = &row{t}
-	return t.format.compileFormat()
+	return t.format.compileUnbound()
 }
 
 // setTablePaths gives every table the path a selector on it is written at, before a
